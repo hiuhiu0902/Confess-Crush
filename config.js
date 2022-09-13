6ed6f2355be181e7b10ec7f9ce9d61665fdc11a7
hiuhiu0902 <113414282+hiuhiu0902@users.noreply.github.com>
2022-09-13 22:51:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Love u so much my angel!! <3 ",
-    introTitle: 'Nguyễn Phươn Quỳnh Trâm',
+    introTitle: 'Nguyễn Phương Quỳnh Trâm',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -10,11 +10,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    question: 'Không biết liệu nàng công chúa của chúng ta có thể để tui làm hoàng tử của cậu hemmm?',
+    btnReply: 'Câu trả lời:',
+    reply: 'Chắc chắn là có rồiii <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Hôm nào rảnh đi xem phim nhaa <3',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
